ed1448891258d9f92f2461641797b60ecc8b670d
Giorgos Tharropoulos <tharropoulos@gmail.com>
2021-05-21 19:39:57 +0300

    Initialized df in Frame

--- src/frame_repository/frame.py
+++ src/frame_repository/frame.py
@@ -7,6 +7,7 @@ from .load_strategy.load_strategy import LoadStrategy
 
 class Frame():
     load_strategy: LoadStrategy
+    df = None
 
     def read_data(self, path: os.PathLike):
         self.df: pd.DataFrame = self.load_strategy.get_data(
